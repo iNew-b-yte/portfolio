c8cd7cc259cd0d262da938576b9497ad29343b5a
Naveen Personal <naveenbrothers26@gmail.com>
2025-12-17 17:02:12 +0530

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Box } from '@chakra-ui/react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
